bca2d5892aeab8cf1423f65532774bbe4b8631d4
rbartosinski <r.bartosinski@wp.pl>
2021-08-07 15:25:34 +0200

    Sat. 7.08, after second part

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,13 +5,11 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    # path('', views.IndexView.as_view(), name='index'),
-    path('', views.index, name='index'),
-    path('create/', views.create_question, name='create'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:question_id>/edit/', views.edit_question, name='edit'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:question_id>/', views.DetailView.as_view(), name='detail'),
+    path('<int:question_id>/edit/', views.EditQuestionView.as_view(), name='edit'),
+    path('<int:question_id>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.VoteView.as_view(), name='vote'),
 ]
 
 # urlpatterns = [
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,104 +1,95 @@
+from django.contrib import messages
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.utils import timezone
-from django.views import generic
+from django.views import generic, View
 
 from .models import Question, Choice
 from .forms import QuestionForm
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')
-    form = QuestionForm()
-    context = {
-        'form': form,
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(View):
 
-
-def create_question(request):
-    prefix = "My object"
-    form_template = QuestionForm()
-    print("Form template: ", form_template)
-    print("Request POST data: ", request.POST)
-    form = QuestionForm(request.POST)
-    print("Form with request data: ", form)
-
-    # Question.objects.create(
-    #     question_text="{} - {}".format(prefix, request.POST["question_text"]),
-    #     pub_date=request.POST["pub_date"]
-    # )
-
-    if form.is_valid():
-        print("Form from cleaned data dict: ", form.cleaned_data)
-        # Question.objects.create(**form.cleaned_data)
-        Question.objects.create(
-            question_text="{} - {}".format(prefix, form.cleaned_data["question_text"]),
-            pub_date=form.cleaned_data["pub_date"]
-        )
-    else:
-        latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    def get(self, request):
+        latest_question_list = Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
+        form = QuestionForm()
         context = {
             'form': form,
             'latest_question_list': latest_question_list,
-            'error_message': "Form is not valid"
         }
         return render(request, 'polls/index.html', context)
-    return HttpResponseRedirect(reverse('polls:index'))
+
+    def post(self, request):
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            Question.objects.create(**form.cleaned_data)
+        else:
+            messages.error(request, 'Message: Form is not valid')
+            HttpResponseRedirect(reverse('polls:index'))
+        return HttpResponseRedirect(reverse('polls:index'))
 
 
-def edit_question(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    form = QuestionForm(initial={
-        'question_text': question.question_text,
-        'pub_date': question.pub_date,
-    })
-    context = {
-        'form': form,
-        'question': question,
-    }
-    return render(request, 'polls/edit.html', context)
+class EditQuestionView(View):
+
+    def get(self, request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+        form = QuestionForm(initial={
+            'question_text': question.question_text,
+            'pub_date': question.pub_date,
+        })
+        context = {
+            'form': form,
+            'question': question,
+        }
+        return render(request, 'polls/edit.html', context)
+
+    def post(self, request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question.question_text = form.cleaned_data["question_text"]
+            question.pub_date = form.cleaned_data["pub_date"]
+            question.save()
+        else:
+            messages.error(request, 'Message: Form is not valid')
+            HttpResponseRedirect(reverse('polls:edit'))
+        return HttpResponseRedirect(reverse('polls:detail', args=(question.id,)))
 
 
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
+class DetailView(View):
 
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
-
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
-
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    print(question)
-    print("POST: ", request.POST)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-        print("Choice nr: ", request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
+    def get(self, request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
         context = {
             'question': question,
-            'error_message': "You didn't select a choice.",
         }
         return render(request, 'polls/detail.html', context)
-    else:
-        print("var - selected_choice: ", selected_choice)
-        print("var - selected_choice votes: ", selected_choice.votes)
-        selected_choice.votes += 1
-        # selected_choice.choice_text = "Słoń"
-        selected_choice.save()
-        print("var - selected_choice votes - after: ", selected_choice.votes)
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+class ResultsView(View):
+
+    def get(self, request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+        context = {
+            'question': question,
+        }
+        return render(request, 'polls/results.html', context)
+
+
+class VoteView(View):
+
+    def post(self, request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        except (KeyError, Choice.DoesNotExist):
+            messages.error(request, 'Message: You didnt select a choice.')
+            return HttpResponseRedirect(reverse('polls:detail', args=(question.id,)))
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+            return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
 # class IndexView(generic.ListView):
@@ -106,7 +97,11 @@ def vote(request, question_id):
 #     context_object_name = 'latest_question_list'
 #
 #     def get_queryset(self):
-#         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+#         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
+
+# class ResultsView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/results.html'
 
 # def index(request):
 #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -132,3 +127,65 @@ def vote(request, question_id):
 #         'question': question,
 #     }
 #     return render(request, 'polls/results.html', context)
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')
+#     form = QuestionForm()
+#     context = {
+#         'form': form,
+#         'latest_question_list': latest_question_list,
+#     }
+#     return render(request, 'polls/index.html', context)
+
+
+# def create_question(request):
+#     prefix = "My object"
+#     form_template = QuestionForm()
+#     print("Form template: ", form_template)
+#     print("Request POST data: ", request.POST)
+#     form = QuestionForm(request.POST)
+#     print("Form with request data: ", form)
+#
+#     # Question.objects.create(
+#     #     question_text="{} - {}".format(prefix, request.POST["question_text"]),
+#     #     pub_date=request.POST["pub_date"]
+#     # )
+#
+#     if form.is_valid():
+#         print("Form from cleaned data dict: ", form.cleaned_data)
+#         # Question.objects.create(**form.cleaned_data)
+#         Question.objects.create(
+#             question_text="{} - {}".format(prefix, form.cleaned_data["question_text"]),
+#             pub_date=form.cleaned_data["pub_date"]
+#         )
+#     else:
+#         latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#         context = {
+#             'form': form,
+#             'latest_question_list': latest_question_list,
+#             'error_message': "Form is not valid"
+#         }
+#         return render(request, 'polls/index.html', context)
+#     return HttpResponseRedirect(reverse('polls:index'))
+
+# def vote(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     print(question)
+#     print("POST: ", request.POST)
+#     try:
+#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+#         print("Choice nr: ", request.POST['choice'])
+#     except (KeyError, Choice.DoesNotExist):
+#         context = {
+#             'question': question,
+#             'error_message': "You didn't select a choice.",
+#         }
+#         return render(request, 'polls/detail.html', context)
+#     else:
+#         print("var - selected_choice: ", selected_choice)
+#         print("var - selected_choice votes: ", selected_choice.votes)
+#         selected_choice.votes += 1
+#         # selected_choice.choice_text = "Słoń"
+#         selected_choice.save()
+#         print("var - selected_choice votes - after: ", selected_choice.votes)
+#         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
